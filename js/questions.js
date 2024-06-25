ab8579f7db4a1e6dcd152149569409562fa0b10b
aa.cloudengineer <aa.cloudengineer@gmail.com>
2024-06-25 03:45:41 +0100

    first commit

---
+++ js/questions.js
@@ -0,0 +1,118 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+  numb: 1, 	
+  question: "Muslims will often write the initials P.B.U.H. after the name of a prophet. What does this mean?",
+  answer: "Peace be upon him",
+  options: [  
+      "Please be utterly happy",
+      "Peace be upon him", 
+      "Peace be under heaven", 
+      "Peace be up high"
+  ]
+},
+{
+  numb: 2, 	
+  question: "In which direction do Muslims face when praying?",
+  answer: "Towards Mecca",
+  options: [
+    "Towards Mecca",
+    "Towards Istanbul",
+    "Towards Rome",
+    "Towards Jerusalem"
+  ]
+  
+},
+{
+  numb: 3, 
+  question: "The name Islam comes from the word 'Salaam', which means .......",
+  answer: "peace",
+  options: [	
+     "love",
+     "freedom",
+     "peace",
+     "justice"
+  ]
+},
+{
+  numb: 4, 
+  question: "The holy month of prayer, fasting and pilgrimage is known as .......",
+  answer: "Ramadan",
+  options: [
+    "Spring",
+    "Eid",
+    "Ramadan",
+    "Shahadah"
+  ]
+},
+{
+  numb: 5, 
+  question: "How many times a day should Muslims pray?",
+  answer: "Five times",
+  options: [
+     "Once",
+     "Twice",
+     "Four times",
+     "Five times"
+  ]
+ },
+{
+  numb: 6, 
+  question: "Which of the following is NOT a holy and sacred book in Islam?",
+  answer: "De Anima", 
+  options: [
+       "The Koran",
+       "The Psalms",
+       "The Bible",
+       "De Anima"
+  ]
+  
+},
+{
+  numb: 7, 
+  question: "Where do Muslims go to worship?",
+  answer: "A mosque",
+  options: [
+     "A temple",
+     "A church",
+     "A synagogue",
+     "A mosque"
+  ]
+},
+{
+  numb: 8,
+  question: "How many pillars of Islam are there?",
+  answer: "Five",
+  options: [
+     "Four",
+     "Five",
+     "Eight", 
+     "Ten"
+  ]
+
+},
+{
+  numb: 9,
+  question: "One of the pillars of Islam is Salah, which means .......",
+  answer: "prayer",
+  options: [
+    "prayer",
+    "fasting", 
+    "giving to charity",
+    "pilgrimage"
+  ]
+ 
+},
+{
+  numb: 10,
+  question: "What is the name for the pilgrimage to Mecca which all faithful Muslims must try to undertake at least once?",
+  answer: "Hajj",
+  options: [
+    "Shahadah", 
+    "Salah",
+    "Hajj",
+    "Zakah"
+  ]
+  
+},
+];
