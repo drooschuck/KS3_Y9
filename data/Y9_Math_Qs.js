a38bb8970804f12e439c9c5335df054c664584bf
drooschuck <114895991+drooschuck@users.noreply.github.com>
2025-04-09 22:40:28 +0100

    Update Y9_Math_Qs.js

--- data/Y9_Math_Qs.js
+++ data/Y9_Math_Qs.js
@@ -1,1 +1,114 @@
+let questions = [
+  {
+    numb: 1,
+    question: "What is 2^3?",
+    answer: "8",
+    options: [
+      "6",
+      "7",
+      "8",
+      "9"
+    ]
+  },
+  {
+    numb: 2,
+    question: "Convert 4500 into standard form.",
+    answer: "4.5 x 10^3",
+    options: [
+      "4.5 x 10^2",
+      "45 x 10^2",
+      "4.5 x 10^3",
+      "0.45 x 10^4"
+    ]
+  },
+  {
+    numb: 3,
+    question: "What is the value of 5^0?",
+    answer: "1",
+    options: [
+      "0",
+      "1",
+      "5",
+      "10"
+    ]
+  },
+  {
+    numb: 4,
+    question: "Simplify: (x^2)(x^3)",
+    answer: "x^5",
+    options: [
+      "x^6",
+      "x^5",
+      "x^3",
+      "x^2"
+    ]
+  },
+  {
+    numb: 5,
+    question: "If 3x + 5 = 20, what is the value of x?",
+    answer: "5",
+    options: [
+      "3",
+      "5",
+      "7",
+      "15"
+    ]
+  },
+  {
+    numb: 6,
+    question: "What is the average of the following numbers: 4, 8, 6, 10?",
+    answer: "7",
+    options: [
+      "6",
+      "7",
+      "8",
+      "9"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Expand the expression: (x + 3)(x + 2)",
+    answer: "x^2 + 5x + 6",
+    options: [
+      "x^2 + 5x + 6",
+      "x^2 + 6x + 5",
+      "x^2 + 3x + 2",
+      "x^2 + 6"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Rearrange the formula A = l × w to find w.",
+    answer: "w = A/l",
+    options: [
+      "w = A/l",
+      "l = A/w",
+      "A = w/l",
+      "w = l/A"
+    ]
+  },
+  {
+    numb: 9,
+    question: "What is the median of the following data set: 3, 7, 9, 5, 11?",
+    answer: "7",
+    options: [
+      "5",
+      "7",
+      "9",
+      "11"
+    ]
+  },
+  {
+    numb: 10,
+    question: "If you want to plan a survey, which of the following is the first step?",
+    answer: "Define the purpose of the survey",
+    options: [
+      "Collect data",
+      "Analyze data",
+      "Define the purpose of the survey",
+      "Present results"
+    ]
+  }
+];
 
+export { questions };
