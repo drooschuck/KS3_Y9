5be95098857ed9dc63f9e0c71c22b259ca333a8a
drooschuck <114895991+drooschuck@users.noreply.github.com>
2025-04-09 03:31:28 +0100

    Update cs_questions.js

--- js/cs_questions.js
+++ js/cs_questions.js
@@ -1,5 +1,5 @@
 // creating an array and passing the number, questions, options, and answers
-let questions = [
+export let questions = [
   {
     numb: 1,
     question: "Which of the following is a guideline for keeping your identity secure on the Internet?",
